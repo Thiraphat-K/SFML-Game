2af1ed994526b04097e0be874ca58891b1df86e5
Thiraphat Ketsingnoi <63010467@kmitl.ac.th>
2020-11-23 22:20:33 +0700

    Stage2(not complete)

--- GAME/GAME/Source.cpp
+++ GAME/GAME/Source.cpp
@@ -23,7 +23,7 @@ int main()
 
 	//init state
 	bool checkGameOpen = false;
-	int Gamestate;
+	int Game_State;
 	while (window.isOpen())
 	{
 		sf::Event event;
@@ -46,7 +46,7 @@ int main()
 					case 0 :
 						cout << "Start has been pressed" << endl;
 						//go to state
-						Gamestate = 1;
+						Game_State = 1;
 						checkGameOpen = true;
 						break;
 					case 1 :
@@ -76,9 +76,11 @@ int main()
 			break;
 	}
 
+	//-----------------Loop Game Stage------------------//
 	while (true)
 	{
-		if (Gamestate == 1)
+		//--Stage 1--//
+		if (Game_State == 1)
 		{
 			sf::View view(sf::Vector2f(1.0f, 1.0f), sf::Vector2f(VIEW_HEIGHT, VIEW_HEIGHT));
 
@@ -115,6 +117,8 @@ int main()
 			sf::Texture box2;
 			std::vector<ObjColli>Objs2, Objs3;
 			Objs2.push_back(ObjColli(&box2, sf::Vector2f(48.0f, 48.0f), sf::Vector2f(750.0f, 648.0f)));
+
+			//---Limit Stage---//
 			Objs3.push_back(ObjColli(&box2, sf::Vector2f(2.0f, 5000.0f), sf::Vector2f(-268.0f, 724.f)));
 			Objs3.push_back(ObjColli(&box2, sf::Vector2f(2.0f, 5000.0f), sf::Vector2f(5890.f, 724.f)));
 
@@ -180,7 +184,7 @@ int main()
 				Compman.Update(deltaTime);
 				sf::Vector2f direction;
 
-				//--Collider Update--//
+				//--Collider_Update--//
 				Collider c = Compman.GetCollider();
 				for (ObjColli& Obj : Objs1)
 					if (Obj.GetCollider().CheckCollision(c, direction, 1.0f))
@@ -189,10 +193,11 @@ int main()
 					if (Obj.GetCollider().CheckCollision(c, direction, 1.0f))
 						Compman.OnCollision(direction);
 
-				//--hitboxtest Update--//
+				//--hitboxtest_Update--//
 				hitbox1.Update(-22.0, -37.0, Compman.GetPosition());
 				hitbox0.Update(Objs2[0].GetSize().x, Objs2[0].GetSize().y, Objs2[0].GetPosition());
 
+				//----------Set_View----------//
 				view.setCenter(Compman.GetPosition().x, Compman.GetPosition().y);
 				if (view.getCenter().x - 195.0f <= 0.0f)
 				{
@@ -226,7 +231,7 @@ int main()
 				}
 				//cout << view.getCenter().x << "\t" << view.getCenter().y << endl ;
 
-				window.clear(sf::Color(255, 255, 255));
+				window.clear(sf::Color(210, 210, 210));
 				//--DrawEverythings--//
 				window.draw(bg);
 				window.setView(view);
@@ -241,6 +246,144 @@ int main()
 				text1.drawtext((int)abs(PointTime), (string)"Point : ", (string)"", sf::Vector2f(view.getCenter().x + (window.getSize().x / 2) - 200, view.getCenter().y - (window.getSize().y / 2) + 55), window, sf::Color(255, 150, 0));
 				window.display();
 			}
+
+			////--Stage 2--//
+			//if (Game_State == 2)
+			//{
+			//	sf::View view(sf::Vector2f(1.0f, 1.0f), sf::Vector2f(VIEW_HEIGHT, VIEW_HEIGHT));
+
+			//	//***Background**//
+			//	sf::Texture Bg;
+			//	Bg.loadFromFile("Object/grid.png");
+			//	sf::Sprite bg(Bg);
+			//	bg.setScale(1.2, 1.2);
+			//	bg.setPosition(2048.f, -55.f);
+
+			//	//***BoxStage**//
+			//	sf::Texture box;
+			//	box.loadFromFile("Object/box.png");
+			//	std::vector<ObjColli>Objs1;
+			//	Objs1.push_back(ObjColli(&box, sf::Vector2f(boxes, boxes), sf::Vector2f(0.0f, 1000.0f)));
+			//	Objs1.push_back(ObjColli(&box, sf::Vector2f(boxes, boxes), sf::Vector2f(0.0f + boxes, 1000.0f)));
+			//	Objs1.push_back(ObjColli(&box, sf::Vector2f(boxes, boxes), sf::Vector2f(0.0f + boxes * 2, 1000.0f)));
+			//	Objs1.push_back(ObjColli(&box, sf::Vector2f(boxes, boxes), sf::Vector2f(0.0f + boxes * 3, 1000.0f)));
+			//	Objs1.push_back(ObjColli(&box, sf::Vector2f(boxes, boxes), sf::Vector2f(0.0f + boxes * 4, 1000.0f)));
+			//	Objs1.push_back(ObjColli(&box, sf::Vector2f(boxes, boxes), sf::Vector2f(0.0f + boxes * 5, 1000.0f)));
+			//	Objs1.push_back(ObjColli(&box, sf::Vector2f(boxes, boxes), sf::Vector2f(0.0f + boxes * 6, 1000.0f)));
+			//	Objs1.push_back(ObjColli(&box, sf::Vector2f(boxes, boxes), sf::Vector2f(0.0f + boxes * 7, 1000.0f)));
+			//	Objs1.push_back(ObjColli(&box, sf::Vector2f(boxes, boxes), sf::Vector2f(0.0f + boxes * 8, 1000.0f)));
+			//	Objs1.push_back(ObjColli(&box, sf::Vector2f(boxes, boxes), sf::Vector2f(0.0f + boxes * 9, 1000.0f)));
+			//	Objs1.push_back(ObjColli(&box, sf::Vector2f(boxes, boxes), sf::Vector2f(0.0f + boxes * 10, 1000.0f)));
+			//	Objs1.push_back(ObjColli(&box, sf::Vector2f(boxes, boxes), sf::Vector2f(0.0f + boxes * 11, 1000.0f)));
+
+			//	//---Limit Stage---//
+			//	Objs3.push_back(ObjColli(&box2, sf::Vector2f(2.0f, 5000.0f), sf::Vector2f(-268.0f, 724.f)));
+			//	Objs3.push_back(ObjColli(&box2, sf::Vector2f(2.0f, 5000.0f), sf::Vector2f(5890.f, 724.f)));
+
+			//	float deltaTime = 1500.0f;
+			//	sf::Clock clock;
+			//	float timeElasped = 0;
+			//	float GameTime = 10;
+			//	float PointTime = 100;
+
+			//	while (window.isOpen())
+			//	{
+			//		timeElasped += deltaTime;
+			//		deltaTime = clock.restart().asSeconds();
+			//		if (deltaTime > 1.0f / 20.0f)
+			//			deltaTime = 1.0f / 20.0f;
+			//		if (GameTime > 0)
+			//			GameTime -= deltaTime;
+			//		if (GameTime <= 0)
+			//			PointTime -= deltaTime;
+			//		if (PointTime <= 0)
+			//			PointTime = 0;
+			//		sf::Event event;
+			//		while (window.pollEvent(event))
+			//		{
+			//			switch (event.type)
+			//			{
+			//			case sf::Event::Closed:
+			//				window.close();
+			//				break;
+			//			case sf::Event::Resized:
+			//				ResizeView(window, view);
+			//				break;
+			//			case sf::Event::MouseButtonPressed:
+			//				cout << "Mouse button has been pressed" << endl;
+			//				switch (event.key.code)
+			//				{
+			//				case sf::Mouse::Left:
+			//					cout << "LEFT KEY has been pressed" << endl;
+			//					break;
+			//				}
+			//				break;
+			//			case sf::Event::MouseButtonReleased:
+			//				cout << "Mouse button has been released" << endl;
+			//				break;
+			//			}
+			//			//cout << "Position x : " << Compman.getPosition().x << "\n" << "Position y : " << Compman.getPosition().y << "\n" << endl;
+
+			//			if (sf::Keyboard::isKeyPressed(sf::Keyboard::P))
+			//			{
+			//				window.close();
+			//			}
+			//		}
+			//		sf::Vector2f direction;
+
+			//		//--Collider Update--//
+			//		Collider c = Compman.GetCollider();
+			//		for (ObjColli& Obj : Objs1)
+			//			if (Obj.GetCollider().CheckCollision(c, direction, 1.0f))
+			//				Compman.OnCollision(direction);
+			//		for (ObjColli& Obj : Objs3)
+			//			if (Obj.GetCollider().CheckCollision(c, direction, 1.0f))
+			//				Compman.OnCollision(direction);
+			//		//----------Set_View----------//
+			//		view.setCenter(Compman.GetPosition().x, Compman.GetPosition().y);
+			//		if (view.getCenter().x - 195.0f <= 0.0f)
+			//		{
+			//			if (view.getCenter().y - 450.0f <= 0.0f)
+			//			{
+			//				view.setCenter(195.f, Compman.GetPosition().y);
+			//			}
+			//			if (view.getCenter().y + 450.0f >= 900.0f)
+			//			{
+			//				view.setCenter(195.f, Compman.GetPosition().y);
+			//			}
+			//			if (view.getCenter().y - 450.0f > 0.0f && view.getCenter().y + 450.0f < 900.0f)
+			//			{
+			//				view.setCenter(195.f, Compman.GetPosition().y);
+			//			}
+			//		}
+			//		if (view.getCenter().x + 415.0f >= 5850.f)
+			//		{
+			//			if (view.getCenter().y - 450.0f <= 0.0f)
+			//			{
+			//				view.setCenter(5435.f, Compman.GetPosition().y);
+			//			}
+			//			if (view.getCenter().y + 450.0f >= 900.0f)
+			//			{
+			//				view.setCenter(5435.f, Compman.GetPosition().y);
+			//			}
+			//			if (view.getCenter().y - 450.0f > 0.0f && view.getCenter().y + 450.0f < 900.0f)
+			//			{
+			//				view.setCenter(5435.f, Compman.GetPosition().y);
+			//			}
+			//		}
+			//		//cout << view.getCenter().x << "\t" << view.getCenter().y << endl ;
+
+			//		window.clear(sf::Color(210, 210, 210));
+			//		//--DrawEverythings--//
+			//		window.draw(bg);
+			//		window.setView(view);
+			//		Compman.Draw(window);
+			//		for (ObjColli& Obj : Objs1)
+			//			Obj.Draw(window);
+			//		for (ObjColli& Obj : Objs3)
+			//			Obj.Draw(window);
+			//		window.display();
+			//}
 		}
 
 	}
